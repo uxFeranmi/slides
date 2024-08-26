0d7dfa10f6e2d211c97442574013937fc03c1a7f
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2024-08-26 02:56:57 +0100

    build

---
+++ build/_next/static/Me360pA__dqunWL8KuJIk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-1c0c0ea27bdb1a9c.js"],"/_error":["static/chunks/pages/_error-1be831200e60c5c0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	distDir: 'build',
 	pageExtensions: ['page.tsx', 'm.ts'],
+	output: 'export',
 };
 
 export default nextConfig;
